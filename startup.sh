9fb10e51eaae9d07d60c5ef8375e1af9e4f4da67
Hans Zehner <hans@dcms.at>
2021-01-13 13:21:34 +0100

    add firebird.conf TMP Cache

--- startup.sh
+++ startup.sh
@@ -2,7 +2,9 @@
 
 /opt/firebird/bin/gsec -modify SYSDBA -pw `echo $SYSDBAPASS`
 
+echo "TempBlockSize = $TMPBLOCKSIZE" >> /opt/firebird/firebird.conf 
+echo "TempCacheLimit = $TMPCACHELIMIT" >> /opt/firebird/firebird.conf
 echo "RemoteServicePort = $FIREBIRD_PORT" >> /opt/firebird/firebird.conf 
 echo "RemoteAuxPort = $REMOTE_AUX_PORT" >> /opt/firebird/firebird.conf
 
-/opt/firebird/bin/firebird
+/opt/firebird/bin/fbguard
